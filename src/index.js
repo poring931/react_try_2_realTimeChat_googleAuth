7ff4ff8aa4302a9823081cbc1e1be47079fba1e7
poring931 <poring.m@mail.ru>
2021-12-26 20:42:01 +0300

    Firebase real-time chat

--- src/index.js
+++ src/index.js
@@ -17,13 +17,7 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBgrcTlotabFxkYemvplhluKpnF5dif9Ao",
-  authDomain: "chat-react-ded82.firebaseapp.com",
-  projectId: "chat-react-ded82",
-  storageBucket: "chat-react-ded82.appspot.com",
-  messagingSenderId: "693261415416",
-  appId: "1:693261415416:web:97f65ca690ed3fa5e01c0d",
-  measurementId: "G-CR2PZZXWCJ"
+
 };
 
 // Initialize Firebase
